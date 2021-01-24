8a00ef8047100b63c4d74272f435db39404ea7fe
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-01-24 11:33:02 -0600

    feat: refactor method that returns retrieved pokemon to accept an array of url's to extract pokemon by generation. Refactor to use switch statement to place retrieved Generation into their appropriate state. Created a method to take in a Generation and construct JSX to display the passed Generation

--- src/components/GetPokemon.js
+++ src/components/GetPokemon.js
@@ -6,6 +6,14 @@ function GetPokemon() {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [AllPokemon, setAllPokemon] = useState([]);
+    const [Gen2Pokemon, setGen2Pokemon] = useState([]);
+    const [Gen3Pokemon, setGen3Pokemon] = useState([]);
+    const [Gen4Pokemon, setGen4Pokemon] = useState([]);
+    const [Gen5Pokemon, setGen5Pokemon] = useState([]);
+    const [Gen6Pokemon, setGen6Pokemon] = useState([]);
+    const [Gen7Pokemon, setGen7Pokemon] = useState([]);
+    const [Gen8Pokemon, setGen8Pokemon] = useState([]);
+
 
     //refactor to make a get by poke gen and probably change filter by type to filter by gen...
 
@@ -14,7 +22,7 @@ function GetPokemon() {
         return new Promise((resolve, reject) => {
             fetch(url)
                 .then(response => response.json())
-                .then(data=> {
+                .then(data => {
                     resolve(data)
                 })
         });
@@ -24,21 +32,20 @@ function GetPokemon() {
         const getData = async () => {
             const pokeUrls = [
                 "https://pokeapi.co/api/v2/pokemon?limit=151",
-                "https://pokeapi.co/api/v2/pokemon?offset=151&limit=100"
-                // ,
-                // "https://pokeapi.co/api/v2/pokemon?offset=251&limit=135",
-                // "https://pokeapi.co/api/v2/pokemon?offset=386&limit=108",
-                // "https://pokeapi.co/api/v2/pokemon?offset=495&limit=154",
-                // "https://pokeapi.co/api/v2/pokemon?offset=649&limit=72",
-                // "https://pokeapi.co/api/v2/pokemon?offset=721&limit=88",
-                // "https://pokeapi.co/api/v2/pokemon?offset=809&limit=89",
+                "https://pokeapi.co/api/v2/pokemon?offset=151&limit=100",
+                "https://pokeapi.co/api/v2/pokemon?offset=251&limit=135",
+                "https://pokeapi.co/api/v2/pokemon?offset=386&limit=108",
+                "https://pokeapi.co/api/v2/pokemon?offset=495&limit=154",
+                "https://pokeapi.co/api/v2/pokemon?offset=649&limit=72",
+                "https://pokeapi.co/api/v2/pokemon?offset=721&limit=88",
+                "https://pokeapi.co/api/v2/pokemon?offset=809&limit=89",
             ];
-            // for(let url of pokeUrls) {
-            let url = "https://pokeapi.co/api/v2/pokemon?limit=25";
+            for (let url of pokeUrls) {
+                // let url = "https://pokeapi.co/api/v2/pokemon?limit=151";
                 let response = await getListByGen(url)
                 await renderPokemon(response.results);
                 // setIsLoaded(!isLoaded);
-            // }
+            }
         }
         getData();
     }
@@ -57,14 +64,87 @@ function GetPokemon() {
         });
     }
 
+    let counter = 0;
     const renderPokemon = async data => {
+        // console.log(data);
         let pokeList = await Promise.all(data.map(async pokemon => {
             let getPokemon = await getEachPokemon(byPokeUrl + pokemon.name)
             return getPokemon;
         }))
-        setAllPokemon(pokeList);
+        counter++;
+        switch (counter) {
+            case 1 :
+                setAllPokemon(pokeList);
+                break;
+            case 2 :
+                setGen2Pokemon(pokeList);
+
+                break;
+            case 3 :
+                setGen3Pokemon(pokeList);
+
+                break;
+            case 4 :
+                setGen4Pokemon(pokeList);
+
+                break;
+            case 5 :
+                setGen5Pokemon(pokeList);
+
+                break;
+            case 6 :
+                setGen6Pokemon(pokeList);
+
+                break;
+            case 7 :
+                setGen7Pokemon(pokeList);
+
+                break;
+            case 8 :
+                setGen8Pokemon(pokeList);
+
+                break;
+            default :
+                break;
+        }
+
     }
 
+    const CreatePokeCards = passedPokemon => {
+        return (
+            passedPokemon.map((pokemon, index) => {
+                let type2;
+                if (pokemon.types[1] !== undefined) {
+                    type2 = <div className={pokemon.types[1].type.name}>
+                        {pokemon.types[1].type.name.charAt(0).toUpperCase() + pokemon.types[1].type.name.substr(1)}
+                    </div>
+                } else {
+                }
+                return <div className={"grid-item"} key={index}>
+                    <div className={"left-card"}>
+                        <div className={"image-radius"}>
+                            <img src={pokemon.sprites.front_default} alt="pokeImg"/>
+                        </div>
+                        <p>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.substr(1)}</p>
+                    </div>
+                    <div className={"right-card"}>
+                        <p>Dex Entry: {pokemon.id}</p>
+                        <p>Height: {pokemon.height}"</p>
+                        <p>Weight: {pokemon.weight}lbs</p>
+                        <div>
+                            <p>Type:</p>
+                            <div>
+                                <div className={pokemon.types[0].type.name}>
+                                    {pokemon.types[0].type.name.charAt(0).toUpperCase() + pokemon.types[0].type.name.substr(1)}
+                                </div>
+                                {type2}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            })
+        )
+    }
 
     if (error) {
         return <div>Error: {error.message}</div>;
@@ -73,39 +153,40 @@ function GetPokemon() {
     } else {
 
         return (
-            <div className={"grid-container"}>
-                {AllPokemon.map((pokemon,index) => {
-                    let type2;
-                    if(pokemon.types[1] !== undefined) {
-                        type2 = <div className={pokemon.types[1].type.name}>
-                                            {pokemon.types[1].type.name.charAt(0).toUpperCase() + pokemon.types[1].type.name.substr(1)}
-                                        </div>
-                    }
-                    else {}
-                    return  <div className={"grid-item"} key={index}>
-                                <div className={"left-card"}>
-                                    <div className={"image-radius"}>
-                                        <img src={pokemon.sprites.front_default} alt="pokeImg"/>
-                                    </div>
-                                    <p>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.substr(1)}</p>
-                                </div>
-                                <div className={"right-card"}>
-                                    <p>Dex Entry: {pokemon.id}</p>
-                                    <p>Height: {pokemon.height}"</p>
-                                    <p>Weight: {pokemon.weight}lbs</p>
-                                    <div>
-                                        <p>Type:</p>
-                                        <div>
-                                            <div className={pokemon.types[0].type.name}>
-                                                {pokemon.types[0].type.name.charAt(0).toUpperCase() + pokemon.types[0].type.name.substr(1)}
-                                            </div>
-                                            {type2}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                })}
-            </div>
+            <main>
+                <h4>Generation 1</h4>
+                <section className={"grid-container"}>
+                    {CreatePokeCards(AllPokemon)}
+                </section>
+                <h4>Generation 2</h4>
+                <section className={"grid-container"}>
+                    {CreatePokeCards(Gen2Pokemon)}
+                </section>
+                <h4>Generation 3</h4>
+                <section className={"grid-container"}>
+                    {CreatePokeCards(Gen3Pokemon)}
+                </section>
+                <h4>Generation 4</h4>
+                <section className={"grid-container"}>
+                    {CreatePokeCards(Gen4Pokemon)}
+                </section>
+                <h4>Generation 5</h4>
+                <section className={"grid-container"}>
+                    {CreatePokeCards(Gen5Pokemon)}
+                </section>
+                <h4>Generation 6</h4>
+                <section className={"grid-container"}>
+                    {CreatePokeCards(Gen6Pokemon)}
+                </section>
+                <h4>Generation 7</h4>
+                <section className={"grid-container"}>
+                    {CreatePokeCards(Gen7Pokemon)}
+                </section>
+                <h4>Generation 8</h4>
+                <section className={"grid-container"}>
+                    {CreatePokeCards(Gen8Pokemon)}
+                </section>
+            </main>
         )
     }
 }
