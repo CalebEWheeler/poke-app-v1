8696ed5e56ab82cf62d37cfa6b98fc73f8d124bc
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-01-29 21:58:52 -0600

    feat: implement rounding methods to handle the conversion to meters and kilograms then rounding to the 1st decimal place

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -1,13 +1,19 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 const CardInfo = (id, height, weight, type1, type2) => {
     const capType = (type) => { return type.charAt(0).toUpperCase() + type.substr(1)}
+    // const convertHeight = (height) => {
+    //     return (height * .1).toString().substr(0,3);
+    // }
+    // const convertWeight = (weight) => {
+    //     Math.round(weight * .1)
+    // }
 
     return (
         <div className={"right-card"}>
             <p>Dex Entry: {id}</p>
-            <p>Height: {(height * .1)} meters</p>
-            <p>Weight: {weight * .1} kg</p>
+            <p>Height: {Math.round((height * .1) * 100 + Number.EPSILON) / 100} meters</p>
+            <p>Weight: {Math.round((weight * .1) * 100 + Number.EPSILON) / 100} kg</p>
             <div>
                 <p>Type:</p>
                 <div>
