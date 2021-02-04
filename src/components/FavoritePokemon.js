eda6e17cab14ae2ab2acf51ac1788d14b729373b
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-02-04 13:27:01 -0600

    feat: add dissapointed pikachu gif to 'Favorite Pokemon' component and will be displayed if localStorage.length === 0

--- src/components/FavoritePokemon.js
+++ src/components/FavoritePokemon.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import CreatePokeCards from "./CreatePokeCards";
 import {faMinusSquare} from "@fortawesome/free-solid-svg-icons";
+import pikaGif from "./Images/dissapointedpika.gif";
 
 const FavoritePokemon = () => {
     const [error, setError] = useState(null);
@@ -21,6 +22,15 @@ const FavoritePokemon = () => {
                 <h4>Favorite Pokemon</h4>
                 <section className={"grid-container"}>
                     {CreatePokeCards(favoritePokemon)}
+                </section>
+                <section className={localStorage.length === 0 ?
+                    "dissapointed-pika-section" : "hide-gen-section"}>
+                    <div className={"oh-no-msg-cont"}>
+                        <img className={"pika-img"} src={pikaGif} alt="dissapointedPikachu"/>
+                        <div className={"oh-no-cont"}>
+                            <p className={"oh-no-text"}>Oh no,<br/>it appears<br/>you don't have<br/>any favorite<br/>pokemon...</p>
+                        </div>
+                    </div>
                 </section>
             </main>
         )
