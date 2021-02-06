722b41c35b79b5ec26a520919c9084c5bd7b3174
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-02-05 23:16:55 -0600

    feat: create an about me component and started to input information for the page

---
+++ src/components/Hooks/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const About = () => {
+
+    return (
+        <main>
+            <section>
+                <div>
+                    About
+                </div>
+                <div>
+                    <p>PokeApp is an application built to allow users to see all pokemon from the original 151 to the latest generation from the Galar region!
+                        Want to quickly find a pokemon by their name? Click on the magnifying glass icon and search for your desired pokemon!
+                        An added bonus is that users can favorite pokemon by clicking on the star. Want to see your favorited pokemon? Click on the pokeball to see them all!
+                    </p>
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default About;
